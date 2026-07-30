7eda1347f3aadc02c7104a509d157f3a8caded09
Nishkarsh Kumar <nishkarshkumar@Nishkarshs-MacBook-Air.local>
2026-07-30 07:52:53 +0530

    fix: remove stray requester kwarg from utils.play_log() call

--- ishu/plugins/play.py
+++ ishu/plugins/play.py
@@ -135,14 +135,13 @@ async def play_hndlr(
         )
 
     if await db.is_logger():
-        await utils.play_log(
-            m,
-            sent.link,
-            file.title,
-            file.duration,
-            video=bool(video),
-            requester=m.from_user,
-        )
+       await utils.play_log(
+        m,
+        sent.link,
+        file.title,
+        file.duration,
+        video=bool(video),
+       )
 
     file.user = mention
 
